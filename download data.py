cae9a874b2b5d82c885ae29ebe354b6db5f30cdc
SaranaAbidueva <sara.abidueva2010@gmail.com>
2024-11-28 23:15:20 +0300

    corrected downloading data

--- download data.py
+++ download data.py
@@ -1,11 +1,23 @@
 import kagglehub
 from datetime import date
-
+import shutil
+import os
 
 def get_date():
-    today = date.today().strftime("%Y%m")
-    return today
+    today = date.today()
+    return today.strftime("%Y%m")
 
-# Download the latest version
-path = f"bwandowando/dota-2-pro-league-matches-{get_date()}"
-dataset = kagglehub.dataset_download(path)
+
+def download_data():
+    files_to_download = ['players', 'picks_bans', 'teams', 'main_metadata']
+    dataset_url = f"bwandowando/dota-2-pro-league-matches-2023"
+    for file in files_to_download:
+        tgt_directory = f'data/{dt}'
+        if not os.path.exists(tgt_directory):
+            os.makedirs(tgt_directory)
+        dataset_path = kagglehub.dataset_download(dataset_url, path=f'{dt}/{file}.csv')
+        shutil.copy(dataset_path, tgt_directory)
+
+
+dt = get_date()
+download_data()
