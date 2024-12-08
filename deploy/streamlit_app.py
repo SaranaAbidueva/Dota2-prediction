888ba5e45e62518b9d9cfe2c4284fc40d26099d3
SaranaAbidueva <sara.abidueva2010@gmail.com>
2024-12-08 23:58:11 +0300

    start adding streamlit

--- deploy/streamlit_app.py
+++ deploy/streamlit_app.py
@@ -1,16 +1,25 @@
 import streamlit as st
-
+import pandas as pd
 
 st.title('Predict Dota 2 Pro league game')
 
 st.text_input("Radiant team name", key="radiant_name")
 st.text_input("Dire team name", key="dire_name")
 
-st.text_input("Radiant pos1 hero", key="radiant.1_hero")
+df = pd.DataFrame('Constants.Heroes.csv')
+df['name_short'] = df['name'].apply(lambda x: x[14:])
+
+# Select box for heroes
+for side in ['Radiant', 'Dire']:
+    for i in range(1, 6):
+        option = st.selectbox(f'{side} pos{i} hero', df['name_short'], key=f"{side}.{i}_hero")
+
+for side in ['Radiant', 'Dire']:
+    for i in range(1, 6):
+        option = st.selectbox(f'{side} pos{i} hero', list(range(1, 6)), key=f"{side}.{i}_hero_variant")
+# st.text_input("Radiant pos1 hero", key="radiant.1_hero")
 st.text_input("Dire pos1 hero", key="dire.1_hero")
 
 st.text_input("Radiant pos1 hero variant", key="radiant.1_hero_variant")
 st.text_input("Dire pos1 hero variant", key="dire.1_hero_variant")
 
-
-st.text_input("Radiant team name", key="radiant_name")
